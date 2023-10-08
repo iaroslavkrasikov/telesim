b7a3ebb05717ac47fee21eac1e5b08994163483c
iaroslav <98962283+iaroslavkrasikov@users.noreply.github.com>
2023-10-08 20:09:10 +0300

    ...

--- api/index.py
+++ api/index.py
@@ -1,18 +1,27 @@
-from os import environ as env
-from dotenv import load_dotenv
+from os import getenv as env
+import psycopg
 import telebot
 import flask
 
-load_dotenv("../.env.local")
 
-WEBHOOK_URL = "https://" + env.get("VERCEL_URL")
-if env.get("VERCEL_ENV") == "development":
-	WEBHOOK_URL = env.get("DEV_WEBHOOK_URL")
 
-bot = telebot.TeleBot(env.get("TOKEN"))
-bot.set_webhook(url=WEBHOOK_URL)
 
+# We have development env with dev WEBHOOK_URL and local dev DB
+# And we have Vercel prod env where WEBHOOK_URL=VERCEL_URL.
+# TOKEN is universal but stored only on Vercel env.
+# We check current env, if dev then we add TOKEN to dev env
+env = dotenv_values("../.env.local")
+env["WEBHOOK_URL"] = env["VERCEL_URL"]
+_env_TOKEN = env["TOKEN"]
+if env["VERCEL_ENV"] == "development":
+	env = dotenv_values("../.env.development.local")
+	env["TOKEN"] = _env_TOKEN
+
+
+db = psycopg.connect(env("POSTEGRES_SQL"))
+bot = telebot.TeleBot(env("TOKEN"))
 app = flask.Flask(__name__)
+
 
 @bot.message_handler(func=lambda m: True)
 def send_welcome(message):
@@ -20,7 +29,7 @@ def send_welcome(message):
 
 @app.route("/api")
 def index():
-	return WEBHOOK_URL
+	return env.get("WEBHOOK_URL")
 
 
 @app.route("/api", methods=["POST"])
